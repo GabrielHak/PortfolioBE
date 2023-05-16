e0193d52a5fd416f4a2244401d584e6eb38286f2
Gabi Hak <robertohak270997@gmail.com>
2023-05-16 09:59:31 -0300

    Error Cors Fixed

--- src/main/java/com/gabriel/PortfolioBE/PortfolioBeApplication.java
+++ src/main/java/com/gabriel/PortfolioBE/PortfolioBeApplication.java
@@ -2,12 +2,29 @@ package com.gabriel.PortfolioBE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration(proxyBeanMethods = false)
 @SpringBootApplication
 public class PortfolioBeApplication {
 
     public static void main(String[] args) {
             SpringApplication.run(PortfolioBeApplication.class, args);
     }
-
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return (new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                        registry.addMapping("/**")
+                                .allowedOrigins("*")
+                                .allowedMethods("*")
+                                .allowedHeaders("*");
+                }
+        });
+    }
 }
